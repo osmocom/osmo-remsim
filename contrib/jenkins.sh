c7b5c7aaf26f13ac997cb36e912a22ffa300abed
Harald Welte <laforge@gnumonks.org>
2018-10-14 20:42:56 +0200

    Add contrib/jenkins.sh for build verification
    Change-Id: I09a020966c3903673626e6d6bbe097d4ba69774b

---
+++ contrib/jenkins.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# jenkins build helper script for libosmo-abis.  This is how we build on jenkins.osmocom.org
+
+if ! [ -x "$(command -v osmo-build-dep.sh)" ]; then
+	echo "Error: We need to have scripts/osmo-deps.sh from http://git.osmocom.org/osmo-ci/ in PATH !"
+	exit 2
+fi
+
+set -ex
+
+base="$PWD"
+deps="$base/deps"
+inst="$deps/install"
+export deps inst
+
+osmo-clean-workspace.sh
+
+mkdir "$deps" || true
+
+verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
+
+osmo-build-dep.sh libosmocore "" --disable-doxygen
+osmo-build-dep.sh libosmo-abis "" --disable-doxygen
+osmo-build-dep.sh libosmo-netif "" --disable-doxygen
+
+export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="$inst/lib"
+
+set +x
+echo
+echo
+echo
+echo " =============================== libosmo-abis ==============================="
+echo
+set -x
+
+autoreconf --install --force
+./configure --enable-sanitize #--enable-werror
+$MAKE $PARALLEL_MAKE
+$MAKE distcheck \
+  || cat-testlogs.sh
+
+osmo-clean-workspace.sh
