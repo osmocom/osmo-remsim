e0b8d3544387f7c06f6c5acc97a72d7245101705
Oliver Smith <osmith@sysmocom.de>
2024-11-22 13:43:55 +0100

    contrib/jenkins: libosmo-abis after libosmo-netif
    Depends: libosmo-abis I079dc3999de508301dd37ed03e399356a58d3cab
    Depends: libosmo-netif I13d6e88158f6d9ce017986283183ee9c2cc68cae
    Change-Id: I72837fbde099f410eff69362155cb62e8a83c2c6

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -29,8 +29,8 @@ export LD_LIBRARY_PATH="$inst/lib"
 export PATH="$inst/bin:$PATH"
 
 osmo-build-dep.sh libosmocore "" --disable-doxygen
+osmo-build-dep.sh libosmo-netif "" --disable-doxygen
 osmo-build-dep.sh libosmo-abis
-osmo-build-dep.sh libosmo-netif
 osmo-build-dep.sh simtrace2
 
 # Additional configure options and depends
